73c6924c603e19438d815174a307655ec0332332
rahulprops <rahulhack912@gmail.com>
2024-11-21 17:08:48 +0530

    create faculity controller

---
+++ backend/utils/controller/faculity.controller.js
@@ -0,0 +1,25 @@
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+import faculityModal from '../modal/faculity.modal.js'
+import multer from 'multer'
+import handleError from '../middlerare/error_logs/handleError.js'
+ import path from 'path'
+
+ const facuDir= path.join("public/faculity/")
+ const facStore= multer.diskStorage({
+    destination:(req,file,cb)=>{
+        return cb(null , facuDir)
+    },
+    filename:(req,file,cb)=>{
+        return cb(null, file.originalname)
+    }
+ })
+export const faculityMulter=multer({storage:facStore})
+export const createFaculity= async (req,res)=>{
+    const {id}=req.params;
+    const {faculityName ,faculityEmail,faculityPhone, faculityPassword}=req.body;
+    
+    try{}catch(err){
+        return handleError(res,"server chh")
+    }
+}
