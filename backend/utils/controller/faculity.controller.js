87137ee09dcd473656b19e4c42974731b381f2de
rahulprops <rahulhack912@gmail.com>
2024-11-26 21:02:23 +0530

    check admin key valid or not

--- backend/utils/controller/faculity.controller.js
+++ backend/utils/controller/faculity.controller.js
@@ -5,6 +5,7 @@ import multer from "multer";
 import handleError from "../middlerare/error_logs/handleError.js";
 import path from "path";
 import adminModal from "../modal/admin.modal.js";
+import mongoose from "mongoose";
 
 const facuDir = path.join("public/faculity/");
 const facStore = multer.diskStorage({
@@ -28,40 +29,42 @@ export const createFaculity = async (req, res) => {
   if (!faculityPhoto) {
     return handleError(res, 400, "please enter photo");
   }
-//  console.log(faculityPhoto.filename)
+  //  console.log(faculityPhoto.filename)
   if (!faculityName || !faculityEmail || !faculityPhone || !faculityPassword) {
     return handleError(res, 400, "all feilds required ");
   }
 
   // console.log(Id)
   try {
+    if (!mongoose.Types.ObjectId.isValid(Id)) {
+      return handleError(res, 400, "please enter validkey");
+    }
     const checkAdmin = await adminModal.findById(Id);
     // console.log(checkAdmin)
     if (!checkAdmin) {
       return handleError(res, 400, "please enter currect key");
     }
+    console.log("20");
     const faculityfindEmail = await faculityModal.findOne({
       faculityEmail: faculityEmail,
     });
     if (faculityfindEmail) {
       return handleError(res, 400, "email already exits");
     }
-    const hashPass= await bcrypt.hash(faculityPassword,12)
+    const hashPass = await bcrypt.hash(faculityPassword, 12);
     // console.log(hashPass)
-    const createFaculity= new faculityModal({
-        faculityName:faculityName,
-        faculityEmail:faculityEmail,
-        faculityPassword:hashPass,
-        faculityPhoto:faculityPhoto.filename,
-        AdminId:Id
-
-    })
-    if(createFaculity){
-        const faculityCreate= await createFaculity.save()
-        return handleError(res,400,"create sucessful",faculityCreate)
-    }
-    else{
-        return handleError(res,400,"faculity create failed ")
+    const createFaculity = new faculityModal({
+      faculityName: faculityName,
+      faculityEmail: faculityEmail,
+      faculityPassword: hashPass,
+      faculityPhoto: faculityPhoto.filename,
+      AdminId: Id,
+    });
+    if (createFaculity) {
+      const faculityCreate = await createFaculity.save();
+      return handleError(res, 400, "create sucessful", faculityCreate);
+    } else {
+      return handleError(res, 400, "faculity create failed ");
     }
   } catch (err) {
     return handleError(res, 500, `server error ${err}`);
