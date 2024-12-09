3c65510b00930cc8e3779a888611cb422859f247
rahulprops <rahulhack912@gmail.com>
2024-12-09 17:08:30 +0530

    create course done

--- backend/server.js
+++ backend/server.js
@@ -5,13 +5,15 @@ import bodyParser from 'body-parser'
 import adminRouter from './utils/routes/admin.routes.js'
 import cors from 'cors'
 import faculityRouter from './utils/routes/faculity.routes.js'
+import courseRouter from './utils/routes/course.routers.js'
 const app=express()
 const port =process.env.PORT || 3000
 app.use(bodyParser.urlencoded({extended:false}))
-app.use(bodyParser.json())
+app.use(bodyParser.json()) 
 app.use(cors("*"))
 app.use("/api/v1",adminRouter)
 app.use("/api/v1",faculityRouter)
+app.use("/api/v1",courseRouter)
 app.listen(port,()=>{
     console.log(`server on runing http://localhost:/${port}`)
     db()
