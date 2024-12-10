12a9b81150cf71726ec9c2f839b067b1d3d7be3a
rahulprops <rahulhack912@gmail.com>
2024-12-10 13:39:02 +0530

    create protact-route

--- backend/utils/routes/faculity.routes.js
+++ backend/utils/routes/faculity.routes.js
@@ -1,8 +1,9 @@
 import express from 'express'
 import { createFaculity, faculityMulter, getCourse, loginFaculity, updateFaculity } from '../controller/faculity.controller.js';
+import protectRoute from '../middlerare/protect-route/protect-route.js';
 const faculityRouter=express.Router();
  faculityRouter.post("/:Id/create-faculty",faculityMulter.single("faculityPhoto"), createFaculity)
  faculityRouter.post("/login",loginFaculity)
  faculityRouter.put("/admin/:AdminId/faculity-update",updateFaculity)
- faculityRouter.get("/get-course/faculity/:fid",getCourse)
+ faculityRouter.get("/get-course/faculity/:fid",protectRoute,getCourse)
   export default faculityRouter;
